4a3ea0b29ae5affe5e4b976b76a7fe5bde1d9b68
beben <bebenprzemek03@gmail.com>
2023-10-24 18:53:44 +0200

    Dodaj pliki projektów.

---
+++ Object-Oriented Programming/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Object_Oriented_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
